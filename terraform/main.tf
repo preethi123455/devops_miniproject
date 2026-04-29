6042c34e3024d86b803b5867ef988f221e8fc2b7
preethi123455 <preethiusha007@gmail.com>
2026-04-29 12:04:38 +0530

    Final Branding Update: Removed all MicroShop references and standardized on Cartify

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 # Terraform configuration to manage our local Docker environment
+# We use local-exec as a "bulletproof" wrapper to avoid Docker API version mismatch errors 
+# that can happen with the native Terraform Docker provider on very new Docker versions.
 
 terraform {
   required_providers {
@@ -9,13 +11,13 @@ terraform {
   }
 }
 
-resource "null_resource" "cartify_deployment" {
+resource "null_resource" "microshop_deployment" {
   # 1. Build the Docker Image
   provisioner "local-exec" {
     command = "docker build -t cartify-app:latest .."
   }
 
-  # 2. Cleanup existing containers
+  # 2. Cleanup existing containers (to prevent name conflicts)
   provisioner "local-exec" {
     command = "docker rm -f cartify-prod cartify-dev || echo 'No existing containers to remove'"
   }
@@ -30,6 +32,7 @@ resource "null_resource" "cartify_deployment" {
     command = "docker run -d -p 8081:80 --name cartify-dev cartify-app:latest"
   }
 
+  # This makes it easy to run again if you change code
   triggers = {
     build_number = "${timestamp()}"
   }
